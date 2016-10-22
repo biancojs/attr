4d5646adb9cde43fda91adc7301454af0afab239
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-10-22 21:32:04 +0200

    tests doc and all the rest

--- index.next.js
+++ index.next.js
@@ -1,28 +1,126 @@
 import domToArray from 'bianco.dom-to-array'
 
 /**
+ * Normalize the return values, in case of a single value we avoid to return an array
+ * @param   { Array } values - list of values we want to return
+ * @returns { Array|String|Boolean } either the whole list of values or the single one found
+ * @private
+ */
+const normalize = values => values.length === 1 ? values[0] : values
+
+/**
+ * Parse all the nodes received to get/remove/check their attributes
+ * @param   { HTMLElement|NodeList|Array } els    - DOM node/s to parse
+ * @param   { String|Array }               name   - name or list of attributes
+ * @param   { String }                     method - method that will be used to parse the attributes
+ * @returns { Array|String } result of the parsing in a list or a single value
+ * @private
+ */
+function parseNodes(els, name, method) {
+  const names = typeof name === 'string' ? [name] : name
+  return normalize(domToArray(els).map(el => {
+    return normalize(names.map(n => el[method](n)))
+  }))
+}
+
+/**
  * Set any attribute on a single or a list of DOM nodes
- * @param   { HTMLElement|Array } els   - DOM node/s to parse
- * @param   { String }            name  - name of the attribute to set
- * @param   { String }            value - the new value of the attribute
- * @returns { Array } the original array of elements passed to this function
+ * @param   { HTMLElement|NodeList|Array } els   - DOM node/s to parse
+ * @param   { String|Object }              name  - either the name of the attribute to set
+ *                                                 or a list of properties as object key - value
+ * @param   { String }                     value - the new value of the attribute (optional)
+ * @returns { HTMLElement|NodeList|Array } the original array of elements passed to this function
+ *
+ * @example
+ *
+ * import { set } from 'bianco.attr'
+ *
+ * const img = document.createElement('img')
+ *
+ * set(img, 'width', 100)
+ *
+ * // or also
+ * set(img, {
+ *   width: 300,
+ *   height: 300
+ * })
+ *
  */
 export function set(els, name, value) {
-  domToArray(els).forEach(el => el.setAttribute(name, value))
+  const attrs = typeof name === 'object' ? name : { [name]: value }
+  const props = Object.keys(attrs)
+
+  domToArray(els).forEach(el => {
+    props.forEach(prop => el.setAttribute(prop, attrs[prop]))
+  })
   return els
 }
 
+/**
+ * Get any attribute from a single or a list of DOM nodes
+ * @param   { HTMLElement|NodeList|Array } els   - DOM node/s to parse
+ * @param   { String|Array }               name  - name or list of attributes to get
+ * @returns { Array|String } list the attributes found
+ *
+ * @example
+ *
+ * import { get } from 'bianco.attr'
+ *
+ * const img = document.createElement('img')
+ *
+ * get(img, 'width') // => '200'
+ *
+ * // or also
+ * get(img, ['width', 'height']) // => ['200', '300']
+ *
+ * // or also
+ * get([img1, img2], ['width', 'height']) // => [['200', '300'], ['500', '200']]
+ */
 export function get(els, name) {
-  return domToArray(els).map(el => el.getAttribute(name))
+  return parseNodes(els, name, 'getAttribute')
 }
 
+/**
+ * Remove any attribute from a single or a list of DOM nodes
+ * @param   { HTMLElement|NodeList|Array } els   - DOM node/s to parse
+ * @param   { String|Array }               name  - name or list of attributes to remove
+ * @returns { HTMLElement|NodeList|Array } the original array of elements passed to this function
+ *
+ * @example
+ *
+ * import { remove } from 'bianco.attr'
+ *
+ * remove(img, 'width') // remove the width attribute
+ *
+ * // or also
+ * remove(img, ['width', 'height']) // remove the width and the height attribute
+ *
+ * // or also
+ * remove([img1, img2], ['width', 'height']) // remove the width and the height attribute from both images
+ */
 export function remove(els, name) {
-  domToArray(els).forEach(el => el.removeAttribute(name))
-  return els
+  return parseNodes(els, name, 'removeAttribute')
 }
 
+/**
+ * Set any attribute on a single or a list of DOM nodes
+ * @param   { HTMLElement|NodeList|Array } els   - DOM node/s to parse
+ * @param   { String|Array }               name  - name or list of attributes to detect
+ * @returns { Boolean|Array } true or false or an array boolean values
+ * @example
+ *
+ * import { has } from 'bianco.attr'
+ *
+ * has(img, 'width') // false
+ *
+ * // or also
+ * has(img, ['width', 'height']) // => [false, false]
+ *
+ * // or also
+ * has([img1, img2], ['width', 'height']) // => [[false, false], [false, false]]
+ */
 export function has(els, name) {
-  return domToArray(els).map(el => el.hasAttribute(name))
+  return parseNodes(els, name, 'hasAttribute')
 }
 
 export default {
--- test.js
+++ test.js
@@ -1,12 +1,130 @@
 require('jsdom-global')()
 const assert = require('assert')
-const { set, get } = require('./')
+const { set, get, remove, has } = require('./')
 const body = document.body
+const $ = document.querySelectorAll.bind(document)
+
 
 describe('Bianco attr', function() {
-  beforeEach(function () {
-    var div = document.createElement('div')
-    div.innerHTML = ``
-    body.appendChild(div)
+
+  it('it can set an attribute', function() {
+    const div = document.createElement('div')
+    set(div, 'hidden', 'val')
+    assert.equal(div.getAttribute('hidden'), 'val')
+  })
+
+  it('it can set an attribute on multiple nodes', function() {
+    const div = document.createElement('div')
+    const span = document.createElement('span')
+    set([div, span], 'hidden', 'val')
+    assert.equal(div.getAttribute('hidden'), 'val')
+    assert.equal(span.getAttribute('hidden'), 'val')
+  })
+
+  it('it can set multiple attributes', function() {
+    const div = document.createElement('div')
+    set(div, {
+      hidden: 'val',
+      canchange: 'val'
+    })
+    assert.equal(div.getAttribute('hidden'), 'val')
+    assert.equal(div.getAttribute('canchange'), 'val')
+  })
+
+  it('it can set multiple attributes on multiple nodes', function() {
+    const div = document.createElement('div')
+    const span = document.createElement('span')
+    set([div, span], {
+      hidden: 'val',
+      canchange: 'val'
+    })
+    assert.equal(div.getAttribute('hidden'), 'val')
+    assert.equal(div.getAttribute('canchange'), 'val')
+
+    assert.equal(span.getAttribute('hidden'), 'val')
+    assert.equal(span.getAttribute('canchange'), 'val')
+  })
+
+  it('it can get attributes', function() {
+    const div = document.createElement('div')
+    set(div, {
+      hidden: 'val',
+      canchange: 'val'
+    })
+    assert.equal(get(div, 'hidden'), 'val')
+    assert.equal(get(div, 'canchange'), 'val')
+  })
+
+  it('it can get attributes on multiple nodes', function() {
+    const div = document.createElement('div')
+    const span = document.createElement('span')
+    set([div, span], {
+      hidden: 'val',
+      canchange: 'val'
+    })
+    assert.deepEqual(get([div, span], 'hidden'), ['val', 'val'])
+    assert.deepEqual(get([div, span], 'canchange'), ['val', 'val'])
+  })
+
+  it('it can get multiple attributes', function() {
+    const div = document.createElement('div')
+    set(div, {
+      hidden: 'val',
+      canchange: 'val'
+    })
+    assert.deepEqual(get(div, ['hidden', 'canchange']), ['val', 'val'])
+  })
+
+  it('it can get multiple attributes on multiple nodes', function() {
+    const div = document.createElement('div')
+    const span = document.createElement('span')
+    set([div, span], {
+      hidden: 'val',
+      canchange: 'val'
+    })
+    assert.deepEqual(get([div, span], ['hidden', 'canchange']), [['val', 'val'], ['val', 'val']])
+  })
+
+  it('it can remove attributes', function() {
+    const div = document.createElement('div')
+    set(div, {
+      hidden: 'val',
+      canchange: 'val'
+    })
+    remove(div, 'hidden')
+    remove(div, 'canchange')
+    assert.deepEqual(get(div, ['hidden', 'canchange']), [null, null])
+  })
+
+  it('it can remove attributes from multiple nodes', function() {
+    const div = document.createElement('div')
+    const span = document.createElement('span')
+    set([div, span], {
+      hidden: 'val',
+      canchange: 'val'
+    })
+    remove([div, span], ['hidden', 'canchange'])
+    assert.deepEqual(get([div, span], ['hidden', 'canchange']), [[null, null], [null, null]])
+  })
+
+  it('it can detect attributes', function() {
+    const div = document.createElement('div')
+    set(div, {
+      hidden: 'val',
+      canchange: 'val'
+    })
+    assert.equal(has(div, 'hidden'), true)
+    assert.deepEqual(has(div, ['hidden', 'canchange', 'foo']), [true, true, false])
+  })
+
+  it('it can detect attributes on multiple nodes', function() {
+    const div = document.createElement('div')
+    const span = document.createElement('span')
+    set([div, span], {
+      hidden: 'val',
+      canchange: 'val'
+    })
+    assert.deepEqual(has([div, span], 'hidden'), [true, true])
+    assert.deepEqual(has([div, span], ['hidden', 'canchange', 'foo']), [[true, true, false], [true, true, false]])
   })
 })
