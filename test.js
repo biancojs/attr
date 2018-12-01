cfb1fd96cb4cb43722b7e8bfc0286f0158d45d6e
Gianluca Guarini <gianluca.guarini@gmail.com>
2018-12-01 18:24:00 +0100

    updated: doc

--- test.js
+++ test.js
@@ -1,26 +1,34 @@
 require('jsdom-global')()
 const assert = require('assert')
-const { set, get, remove, has } = require('./')
+const attr = require('./')
 
 describe('Bianco attr', function() {
+  it('export default contains all the module methods', function() {
+    assert.deepEqual(Object.keys(attr.default), [
+      'get',
+      'set',
+      'remove',
+      'has'
+    ])
+  })
 
   it('it can set an attribute', function() {
     const div = document.createElement('div')
-    set(div, 'hidden', 'val')
+    attr.set(div, 'hidden', 'val')
     assert.equal(div.getAttribute('hidden'), 'val')
   })
 
   it('it can set an attribute on multiple nodes', function() {
     const div = document.createElement('div')
     const span = document.createElement('span')
-    set([div, span], 'hidden', 'val')
+    attr.set([div, span], 'hidden', 'val')
     assert.equal(div.getAttribute('hidden'), 'val')
     assert.equal(span.getAttribute('hidden'), 'val')
   })
 
   it('it can set multiple attributes', function() {
     const div = document.createElement('div')
-    set(div, {
+    attr.set(div, {
       hidden: 'val',
       canchange: 'val'
     })
@@ -31,7 +39,7 @@ describe('Bianco attr', function() {
   it('it can set multiple attributes on multiple nodes', function() {
     const div = document.createElement('div')
     const span = document.createElement('span')
-    set([div, span], {
+    attr.set([div, span], {
       hidden: 'val',
       canchange: 'val'
     })
@@ -44,84 +52,84 @@ describe('Bianco attr', function() {
 
   it('it can get attributes', function() {
     const div = document.createElement('div')
-    set(div, {
+    attr.set(div, {
       hidden: 'val',
       canchange: 'val'
     })
-    assert.equal(get(div, 'hidden'), 'val')
-    assert.equal(get(div, 'canchange'), 'val')
+    assert.equal(attr.get(div, 'hidden'), 'val')
+    assert.equal(attr.get(div, 'canchange'), 'val')
   })
 
   it('it can get attributes on multiple nodes', function() {
     const div = document.createElement('div')
     const span = document.createElement('span')
-    set([div, span], {
+    attr.set([div, span], {
       hidden: 'val',
       canchange: 'val'
     })
-    assert.deepEqual(get([div, span], 'hidden'), ['val', 'val'])
-    assert.deepEqual(get([div, span], 'canchange'), ['val', 'val'])
+    assert.deepEqual(attr.get([div, span], 'hidden'), ['val', 'val'])
+    assert.deepEqual(attr.get([div, span], 'canchange'), ['val', 'val'])
   })
 
   it('it can get multiple attributes', function() {
     const div = document.createElement('div')
-    set(div, {
+    attr.set(div, {
       hidden: 'val',
       canchange: 'val'
     })
-    assert.deepEqual(get(div, ['hidden', 'canchange']), ['val', 'val'])
+    assert.deepEqual(attr.get(div, ['hidden', 'canchange']), ['val', 'val'])
   })
 
   it('it can get multiple attributes on multiple nodes', function() {
     const div = document.createElement('div')
     const span = document.createElement('span')
-    set([div, span], {
+    attr.set([div, span], {
       hidden: 'val',
       canchange: 'val'
     })
-    assert.deepEqual(get([div, span], ['hidden', 'canchange']), [['val', 'val'], ['val', 'val']])
+    assert.deepEqual(attr.get([div, span], ['hidden', 'canchange']), [['val', 'val'], ['val', 'val']])
   })
 
   it('it can remove attributes', function() {
     const div = document.createElement('div')
-    set(div, {
+    attr.set(div, {
       hidden: 'val',
       canchange: 'val'
     })
-    remove(div, 'hidden')
-    remove(div, 'canchange')
-    assert.deepEqual(get(div, ['hidden', 'canchange']), [null, null])
+    attr.remove(div, 'hidden')
+    attr.remove(div, 'canchange')
+    assert.deepEqual(attr.get(div, ['hidden', 'canchange']), [null, null])
   })
 
   it('it can remove attributes from multiple nodes', function() {
     const div = document.createElement('div')
     const span = document.createElement('span')
-    set([div, span], {
+    attr.set([div, span], {
       hidden: 'val',
       canchange: 'val'
     })
-    remove([div, span], ['hidden', 'canchange'])
-    assert.deepEqual(get([div, span], ['hidden', 'canchange']), [[null, null], [null, null]])
+    attr.remove([div, span], ['hidden', 'canchange'])
+    assert.deepEqual(attr.get([div, span], ['hidden', 'canchange']), [[null, null], [null, null]])
   })
 
   it('it can detect attributes', function() {
     const div = document.createElement('div')
-    set(div, {
+    attr.set(div, {
       hidden: 'val',
       canchange: 'val'
     })
-    assert.equal(has(div, 'hidden'), true)
-    assert.deepEqual(has(div, ['hidden', 'canchange', 'foo']), [true, true, false])
+    assert.equal(attr.has(div, 'hidden'), true)
+    assert.deepEqual(attr.has(div, ['hidden', 'canchange', 'foo']), [true, true, false])
   })
 
   it('it can detect attributes on multiple nodes', function() {
     const div = document.createElement('div')
     const span = document.createElement('span')
-    set([div, span], {
+    attr.set([div, span], {
       hidden: 'val',
       canchange: 'val'
     })
-    assert.deepEqual(has([div, span], 'hidden'), [true, true])
-    assert.deepEqual(has([div, span], ['hidden', 'canchange', 'foo']), [[true, true, false], [true, true, false]])
+    assert.deepEqual(attr.has([div, span], 'hidden'), [true, true])
+    assert.deepEqual(attr.has([div, span], ['hidden', 'canchange', 'foo']), [[true, true, false], [true, true, false]])
   })
 })
