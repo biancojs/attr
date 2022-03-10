46710025f1969a39f950e60d92543237064db62e
gianlucaguarini <gianluca.guarini@gmail.com>
2022-03-10 09:30:09 +0100

    updated: improve types

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,20 @@
-export declare function get<T extends Element>(el: T, prop: string): string | null
-export declare function get<T extends Element>(el: T, prop: string[]): string[] | null
-export declare function get<T extends Element>(el: T[], prop: string): string[] | null
-export declare function get<T extends Element>(el: T[], prop: string[]): string[][] | null
+export declare function get<T extends Element>(el: T, attr: string): string | null
+export declare function get<T extends Element>(el: T, attrs: string[]): string[] | null
+export declare function get<T extends Element>(els: T[], attr: string): string[] | null
+export declare function get<T extends Element>(els: T[], attrs: string[]): string[][] | null
 
-export declare function set<T extends Element>(el: T, prop: string): T
-export declare function set<T extends Element>(el: T, prop: string[]): T
-export declare function set<T extends Element>(el: T[], prop: string): T[]
-export declare function set<T extends Element>(el: T[], prop: string[]): T[]
+export declare function set<T extends Element>(el: T, attr: string, val: unknown): T
+export declare function set<T extends Element>(el: T, attrs: Record<string, unknown>): T
+export declare function set<T extends Element>(els: T[], attr: string, val: unknown): T[]
+export declare function set<T extends Element>(els: T[], attrs: Record<string, unknown>): T[]
 
-export declare function remove<T extends Element>(el: T, prop: string): void
-export declare function remove<T extends Element>(el: T, prop: string[]): void
-export declare function remove<T extends Element>(el: T[], prop: string): void
-export declare function remove<T extends Element>(el: T[], prop: string[]): void
+export declare function remove<T extends Element>(el: T, attr: string | string[]): void
+export declare function remove<T extends Element>(els: T[], attr: string | string[]): void
 
-export declare function has<T extends Element>(el: T, prop: string): boolean | null
-export declare function has<T extends Element>(el: T, prop: string[]): boolean[] | null
-export declare function has<T extends Element>(el: T[], prop: string): boolean[] | null
-export declare function has<T extends Element>(el: T[], prop: string[]): boolean[][] | null
+export declare function has<T extends Element>(el: T, attr: string): boolean | null
+export declare function has<T extends Element>(el: T, attrs: string[]): boolean[] | null
+export declare function has<T extends Element>(els: T[], attr: string): boolean[] | null
+export declare function has<T extends Element>(els: T[], attrs: string[]): boolean[][] | null
 
 export default {
   get,
